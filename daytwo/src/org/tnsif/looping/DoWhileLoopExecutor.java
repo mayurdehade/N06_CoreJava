a685758388a5615a050b489b9a45b66d44d2c234
Mayur <mayurdehade0504@gmail.com>
2023-07-24 13:48:29 +0530

    first commit

---
+++ daytwo/src/org/tnsif/looping/DoWhileLoopExecutor.java
@@ -0,0 +1,20 @@
+//Program to demonstrate the do while loop
+package org.tnsif.looping;
+
+public class DoWhileLoopExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 12;
+		do {
+			System.out.println("The value of x is: "+x);
+			x++;
+		} while (x<15);
+		
+		
+		//how many digit in given number
+		//print patter number
+	}
+
+}
---
+++ daytwo/src/org/tnsif/looping/ForLoopExecutor.java
@@ -0,0 +1,26 @@
+//Program to demonstrate for loop
+package org.tnsif.looping;
+
+import java.util.Scanner;
+
+public class ForLoopExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) 
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
