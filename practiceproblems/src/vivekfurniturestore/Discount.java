445c533dc779103073ab4a9537969b3ff7017fd0
Mayur <mayurdehade0504@gmail.com>
2023-09-28 19:47:51 +0530

    commit

--- practiceproblems/src/vivekfurniturestore/Discount.java
+++ practiceproblems/src/vivekfurniturestore/Discount.java
@@ -1,5 +1,15 @@
 package vivekfurniturestore;
 
 public class Discount{
-
+	
+	public double calculateDiscount(Bero obj) {
+		if(obj instanceof SteelBero) {
+			return (obj.getPrice()*10)/100;
+		}
+		else if(obj instanceof WoodenBero) {
+			return (obj.getPrice()*15)/100;
+		}
+		return 0;
+	}
+	
 }
--- practiceproblems/src/vivekfurniturestore/UserInterface.java
+++ practiceproblems/src/vivekfurniturestore/UserInterface.java
@@ -12,11 +12,14 @@ public class UserInterface {
 		System.out.println("Enter Phone Number");
 		long number = sc.nextLong();
 		System.out.println("Enter address");
+		sc.next();
 		String address = sc.nextLine();
 		CustomerDetails c1 = new CustomerDetails(name, number, address);
 		
 		System.out.println("Enter Bero Type");
 		String type = sc.nextLine();
+		
+		Discount d = new Discount();
 		
 		if(type.equals("Wooden Bero")) {
 			System.out.println("Enter Bero Colour");
@@ -24,8 +27,8 @@ public class UserInterface {
 			System.out.println("Enter Wood Type");
 			String wood = sc.nextLine();
 			WoodenBero w = new WoodenBero(type, color, wood);
-			
-			System.out.println("Amount needs to be paid "+w.getPrice());
+			w.calculatePrice();
+			System.out.println("Amount needs to be paid "+(w.getPrice()-w.calculateDiscount(w)));
 		}
 		else if(type.equals("Steel Bero")) {
 			System.out.println("Enter Bero Colour");
@@ -33,8 +36,8 @@ public class UserInterface {
 			System.out.println("Enter Wood Height");
 			int height = sc.nextInt();
 			SteelBero s = new SteelBero(type, color, height);
-			
-			System.out.println("Amount needs to be paid "+s.getPrice());
+			s.calculatePrice();
+			System.out.println("Amount needs to be paid "+(s.getPrice()-s.calculateDiscount(s)));
 		}
 		else {
 			System.out.println(type+" is an invalid bero type");
